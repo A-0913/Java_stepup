ac3293df82eb701a97a527db26a733a08e907102
EC2 Default User <ayaka-31014313@outlook.jp>
2023-01-24 05:14:54 +0000

    応用編1章

---
+++ Chapter1/Main.java
@@ -0,0 +1,29 @@
+// 外部クラス
+class OuterClass {
+    private String str = "OuterClassの変数";
+
+    // 内部クラス
+    class InnerClass {
+        public void innerSample() {
+            System.out.println(str);
+        }
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 外部クラスのインスタンスの生成
+        // ※内部クラスのインスタンスを生成するには、まず外部クラスのインスタンスを生成する
+        OuterClass oc = new OuterClass();
+        // 外部クラスのオブジェクトから内部クラスのインスタンスを生成
+        // ※外部クラスのオブジェクトocから内部クラスのインスタンスを生成し、内部クラスのオブジェクトicに格納する
+        OuterClass.InnerClass ic = oc.new InnerClass();
+        ic.innerSample();
+    }
+
+}
+
+// 内部クラスのインスタンスを生成する方法
+// 1.外部クラス名 外部クラスのオブジェクト名 = new 外部クラス名();
+// 2.外部クラス名.内部クラス名 内部クラスのオブジェクト名 = 外部クラスのオブジェクト名.new 内部クラス名();
