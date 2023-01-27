1c87d25961b5ea7fad0b477a8a0cefa822cdb236
EC2 Default User <ayaka-31014313@outlook.jp>
2023-01-27 05:09:56 +0000

    応用編2章

--- Chapter2/Main3.java
+++ Chapter2/Main3.java
@@ -9,6 +9,7 @@ public class Main3 {
 
   // 2.フルーツの美味しい食べ方を紹介するメソッドを作成する
   public static void howToEat(Fruit fruit) {
+    // Enumはクラスインスタンスのような振る舞いをするが、==を使って比較できる※
     if (fruit == Fruit.Orange) {
         System.out.println("ジュースにするとおいしいよ");
     } else if (fruit == Fruit.Apple) {
@@ -17,6 +18,10 @@ public class Main3 {
         System.out.println("そのままがおいしいよ");
     }
   }
+
+  // ※Enumはクラスインスタンスと同じ扱いができるためequals()メソッドを使った比較も可能
+  // ただし変数"fruit"がnullの場合、「NullPointerException」がスローされるため注意が必要
+  // if (fruit.equals(Fruit.Orange))
 
   // 3.メインメソッドで引数にAppleを選択する
   public static void main(String[] args) {
