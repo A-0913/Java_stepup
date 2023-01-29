cf6417354fc53472d64682635dcbc322ca4d1f70
EC2 Default User <ayaka-31014313@outlook.jp>
2023-01-29 08:48:42 +0000

    応用編3章

--- Chapter3/Main.java
+++ Chapter3/Main.java
@@ -6,48 +6,48 @@ import java.util.List;
 public class Main {
 	public static void main(String[] args) {
 
-        // ArrayListを生成する場合
-        // List<データ型名> オブジェクト名 = new ArrayList<データ型名>();
-        List<String> list1 = new ArrayList<String>();
+    // ArrayListを生成する場合
+    // List<データ型名> オブジェクト名 = new ArrayList<データ型名>();
+    List<String> list1 = new ArrayList<String>();
 
-        // LinkedListを生成する場合に記述
-        // List<データ型名> オブジェクト名 = new ArrayList<データ型名>();
-        // List<Integer> list2 = new LinkedList<Integer>();
+    // LinkedListを生成する場合に記述
+    // List<データ型名> オブジェクト名 = new ArrayList<データ型名>();
+    // List<Integer> list2 = new LinkedList<Integer>();
 
-        // 要素に「りんご」を追加する
-        list1.add("りんご");
-        // 要素に「みかん」を追加する
-        list1.add("みかん");
+    // 要素に「りんご」を追加する
+    list1.add("りんご");
+    // 要素に「みかん」を追加する
+    list1.add("みかん");
 
-        // 位置を指定して要素を追加する
-        list1.add(1, "バナナ");
+    // 位置を指定して要素を追加する
+    list1.add(1, "バナナ");
 
-        // 0に位置する要素を取得する
-        System.out.println(list1.get(0));
-        // 出力結果:りんご
+    // 0に位置する要素を取得する
+    System.out.println(list1.get(0));
+    // 出力結果:りんご
 
-        // 1に位置する要素を取得する
-        System.out.println(list1.get(1));
-        // 出力結果:バナナ
+    // 1に位置する要素を取得する
+    System.out.println(list1.get(1));
+    // 出力結果:バナナ
 
-        // 要素(=バナナ)がリストに含まれるか確認する
-        System.out.println(list1.contains("バナナ"));
-        // 出力結果:true
+    // 要素(=バナナ)がリストに含まれるか確認する
+    System.out.println(list1.contains("バナナ"));
+    // 出力結果:true
 
-        // コレクションの要素数の合計を取得する
-        System.out.println(list1.size());
-        // 出力結果:3
+    // コレクションの要素数の合計を取得する
+    System.out.println(list1.size());
+    // 出力結果:3
 
-        // // 要素(=バナナ)を削除する (要素が文字列の場合、文字列として同じであれば削除される)
-        list1.remove("バナナ");
+    // // 要素(=バナナ)を削除する (要素が文字列の場合、文字列として同じであれば削除される)
+    list1.remove("バナナ");
 
-        // 要素(=バナナ)がリストに含まれるか確認する
-        System.out.println(list1.contains("バナナ"));
-        // 出力結果:false
+    // 要素(=バナナ)がリストに含まれるか確認する
+    System.out.println(list1.contains("バナナ"));
+    // 出力結果:false
 
-        // コレクションの要素数の合計を取得する
-        System.out.println(list1.size());
-        // 出力結果:2
+    // コレクションの要素数の合計を取得する
+    System.out.println(list1.size());
+    // 出力結果:2
 	}
 }
 
