e7af00ccfbe632dbacb2c6e6874d275804d1a4fa
EC2 Default User <ayaka-31014313@outlook.jp>
2023-02-06 08:06:52 +0000

    応用編5章復習

---
+++ Chapter5-2/Main2.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main2 {
+	public static void main(String[] args) {
+		List<String> animals = Arrays.asList("dog", "cat", "fox", "elephant");
+
+		Collections.sort(
+			    animals,
+			    new Comparator<String>() {
+			    // 「Collections.sort(リスト ,コンパレータ )」メソッドの第2引数として匿名クラス(new Comparator<String>()以降)を宣言
+			      @Override
+			      public int compare(String x, String y) {
+			        // 2つのIntegerオブジェクトを数値的に比較する
+			        return x.compareTo(y);
+			        // compareToメソッドの基本構文:変数1.compareTo(変数2)
+			        /* 【実行結果】
+			         負値 : 変数1 < 変数2
+			         0 : 変数1と変数2が等しい
+			         正値 : 変数1 > 変数2
+			        */
+			      }
+			    });
+
+		System.out.println(animals);
+	}
+
+}
