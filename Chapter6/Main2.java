aa4b61331a66a5f4f6809aa7c50c64f384af0d3a
EC2 Default User <ayaka-31014313@outlook.jp>
2023-02-10 01:33:54 +0000

    応用編6章

---
+++ Chapter6/Main2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main2 {
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(1, -8, 4, 2);
+
+		Collections.sort(
+		    numbers,
+		    (Integer x, Integer y) -> {
+		      return Integer.compare(x, y);
+		    });
+
+		System.out.println(numbers);
+	}
+}
