5db916a3c1adbb8da1bf7f3772e62179b808b5da
EC2 Default User <ayaka-31014313@outlook.jp>
2023-02-10 01:39:22 +0000

    応用編6章

--- Chapter6/Main2.java
+++ Chapter6/Main2.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class Main2 {
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(1, -8, 4, 2);
+		// 数値のリスト Arraysを生成し、配列をリストに変換する
+		/* Arrays.asList()メソッド:
+		→　asListの()の中に配列を指定することで、指定した配列からリストを作成して返すメソッド
+		　[基本構文] Arrays.asList(配列);	*/
 
 		Collections.sort(
 		    numbers,
@@ -15,3 +19,19 @@ public class Main2 {
 		System.out.println(numbers);
 	}
 }
+
+/*
+《ラムダ式の基本形》
+	(引数) -> {処理}
+
+《使用例》
+【元のコード】
+  public int compare(Integer x, Integer y) {
+    return Integer.compare(x, y);
+  }
+
+【ラムダ式】
+  (Integer x, Integer y) -> {
+  	return Integer.compare(x, y);
+	};
+*/
